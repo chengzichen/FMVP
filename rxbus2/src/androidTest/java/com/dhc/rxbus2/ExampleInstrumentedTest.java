b85c1675ec399f5c419ee040516a629babca9f88
chengzichen <13340199939@163.com>
2024-08-23 23:17:52 +0800

    update 1.4.2

--- rxbus2/src/androidTest/java/com/dhc/rxbus2/ExampleInstrumentedTest.java
+++ rxbus2/src/androidTest/java/com/dhc/rxbus2/ExampleInstrumentedTest.java
@@ -1,6 +1,8 @@
-package com.dhc.mvp;
+package com.dhc.rxbus2;
 
 import android.content.Context;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +14,13 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
+        assertEquals("com.dhc.rxbus2.test", appContext.getPackageName());
     }
 }
